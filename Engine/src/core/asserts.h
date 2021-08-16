580b6c5d714119218820cadaa8e16f8e1631e97d
Jad Tala <JadTala1621@gmail.com>
2021-08-16 18:21:43 +0100

    Logging and assertions (API)

---
+++ Engine/src/core/asserts.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "defines.h"
+
+// Disable assertions by commenting out the below line.
+#define SLN_ASSERTIONS_ENABLED
+
+#ifdef SLN_ASSERTIONS_ENABLED
+#if _MSC_VER
+#include <intrin.h>
+#define debugBreak() __debugbreak()
+#else
+#define debugBreak() __builtin_trap()
+#endif
+
+SLN_API void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line);
+
+#define SLN_ASSERT(expr)                                                \
+    {                                                                \
+        if (expr) {                                                  \
+        } else {                                                     \
+            report_assertion_failure(#expr, "", __FILE__, __LINE__); \
+            debugBreak();                                            \
+        }                                                            \
+    }
+
+#define SLN_ASSERT_MSG(expr, message)                                        \
+    {                                                                     \
+        if (expr) {                                                       \
+        } else {                                                          \
+            report_assertion_failure(#expr, message, __FILE__, __LINE__); \
+            debugBreak();                                                 \
+        }                                                                 \
+    }
+
+#ifdef _DEBUG
+#define SLN_ASSERT_DEBUG(expr)                                          \
+    {                                                                \
+        if (expr) {                                                  \
+        } else {                                                     \
+            report_assertion_failure(#expr, "", __FILE__, __LINE__); \
+            debugBreak();                                            \
+        }                                                            \
+    }
+#else
+#define SLN_ASSERT_DEBUG(expr)  // Does nothing at all
+#endif
+
+#else
+#define SLN_ASSERT(expr)               // Does nothing at all
+#define SLN_ASSERT_MSG(expr, message)  // Does nothing at all
+#define SLN_ASSERT_DEBUG(expr)         // Does nothing at all
+#endif
--- Testbed/src/main.c
+++ Testbed/src/main.c
@@ -1,7 +1,15 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-  print_int(42);
+    SLN_FATAL("A test message: %f", 3.14f);
+    SLN_ERROR("A test message: %f", 3.14f);
+    SLN_WARN("A test message: %f", 3.14f);
+    SLN_INFO("A test message: %f", 3.14f);
+    SLN_DEBUG("A test message: %f", 3.14f);
+    SLN_TRACE("A test message: %f", 3.14f);
 
-  return 0;
+    SLN_ASSERT(1 == 0);
+
+    return 0;
 }
