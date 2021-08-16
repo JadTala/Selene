11bd3fc250552189c0c6cf85821a363877c2b8b2
Jad Tala <JadTala1621@gmail.com>
2021-08-16 21:54:07 +0100

    Platform layer implementation (API)

--- Testbed/src/main.c
+++ Testbed/src/main.c
@@ -1,6 +1,8 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+#include <platform/platform.h>
+
 int main(void) {
     SLN_FATAL("A test message: %f", 3.14f);
     SLN_ERROR("A test message: %f", 3.14f);
@@ -9,7 +11,13 @@ int main(void) {
     SLN_DEBUG("A test message: %f", 3.14f);
     SLN_TRACE("A test message: %f", 3.14f);
 
-    SLN_ASSERT(1 == 0);
+    platform_state state;
+    if(platform_startup(&state, "Selene Engine Testbed", 100, 100, 1280, 720)) {
+        while(TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 
     return 0;
 }
