03c2bcb3a0c1568d1726b5972043170244340a61
Jad Tala <JadTala1621@gmail.com>
2021-08-20 12:53:44 +0100

    Entry point and application layer (API)

---
+++ Engine/src/entry.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "game_types.h"
+
+#include "core/application.h"
+#include "core/logger.h"
+#include "core/sln_memory.h"
+
+// Externally-defined function to create a game.
+extern b8 create_game(game* out_game);
+
+/**
+ * The main entry point of the application.
+ */
+int main(void) {
+
+    initialize_memory();
+
+    // Request the game instance from the application.
+    game game_inst;
+    if (!create_game(&game_inst)) {
+        SLN_FATAL("Could not create game!");
+        return -1;
+    }
+
+    // Ensure the function pointers exist.
+    if (!game_inst.render || !game_inst.update || !game_inst.initialize || !game_inst.on_resize) {
+        SLN_FATAL("The game's function pointers must be assigned!");
+        return -2;
+    }
+
+    // Initialization.
+    if (!application_create(&game_inst)) {
+        SLN_INFO("Application failed to create!.");
+        return 1;
+    }
+
+    // Begin the game loop.
+    if(!application_run()) {
+        SLN_INFO("Application did not shutdown gracefully.");
+        return 2;
+    }
+
+    shutdown_memory();
+
+    return 0;
+}
---
+++ Testbed/src/entry.c
@@ -0,0 +1,23 @@
+#include "game.h"
+
+#include <entry.h>
+
+#include <core/sln_memory.h>
+
+b8 create_game(game* out_game) {
+    // Application configuration.
+    out_game->app_config.start_pos_x = 240;
+    out_game->app_config.start_pos_y = 220;
+    out_game->app_config.start_width = 1280;
+    out_game->app_config.start_height = 720;
+    out_game->app_config.name = "Selene Engine Testbed";
+    out_game->update = game_update;
+    out_game->render = game_render;
+    out_game->initialize = game_initialize;
+    out_game->on_resize = game_on_resize;
+
+    // Create the game state.
+    out_game->state = sln_allocate(sizeof(game_state), MEMORY_TAG_GAME);
+
+    return TRUE;
+}
