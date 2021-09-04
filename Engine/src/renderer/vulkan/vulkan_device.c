4efc39e32d94416e2fe294c211a2983676cc6b58
Jad Tala <JadTala1621@gmail.com>
2021-09-05 00:06:07 +0100

    Config driven features request (Vulkan)

--- Engine/src/renderer/vulkan/vulkan_device.c
+++ Engine/src/renderer/vulkan/vulkan_device.c
@@ -76,25 +76,37 @@ b8 vulkan_device_create(vulkan_context* context) {
     }
 
     // Request device features.
-    // TODO: should be config driven
     VkPhysicalDeviceFeatures device_features = {};
     device_features.samplerAnisotropy = VK_TRUE;  // Request anistrophy
+
+    b8 portability_required = FALSE;
+    u32 available_extension_count = 0;
+    VkExtensionProperties* available_extensions = 0;
+    VK_CHECK(vkEnumerateDeviceExtensionProperties(context->device.physical_device, 0, &available_extension_count, 0));
+    if (available_extension_count != 0) {
+        available_extensions = sln_allocate(sizeof(VkExtensionProperties) * available_extension_count, MEMORY_TAG_RENDERER);
+        VK_CHECK(vkEnumerateDeviceExtensionProperties(context->device.physical_device, 0, &available_extension_count, available_extensions));
+        for (u32 i = 0; i < available_extension_count; ++i) {
+            if (strings_equal(available_extensions[i].extensionName, "VK_KHR_portability_subset")) {
+                SLN_INFO("Adding required extension 'VK_KHR_portability_subset'.");
+                portability_required = TRUE;
+                break;
+            }
+        }
+    }
+    sln_free(available_extensions, sizeof(VkExtensionProperties) * available_extension_count, MEMORY_TAG_RENDERER);
+
+    u32 extension_count = portability_required ? 2 : 1;
+    const char** extension_names = portability_required
+            ? (const char* [2]) { VK_KHR_SWAPCHAIN_EXTENSION_NAME, "VK_KHR_portability_subset" }
+            : (const char* [1]) { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 
     VkDeviceCreateInfo device_create_info = {VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO};
     device_create_info.queueCreateInfoCount = index_count;
     device_create_info.pQueueCreateInfos = queue_create_infos;
     device_create_info.pEnabledFeatures = &device_features;
-#ifdef SLN_PLATFORM_MACOS
-    device_create_info.enabledExtensionCount = 2;
-    char *extension_names[2];
-    extension_names[0] = VK_KHR_SWAPCHAIN_EXTENSION_NAME;
-    extension_names[1] = "VK_KHR_portability_subset";
-    device_create_info.ppEnabledExtensionNames = (const char *const *)extension_names;
-#else
-    device_create_info.enabledExtensionCount = 1;
-    const char* extension_names = VK_KHR_SWAPCHAIN_EXTENSION_NAME;
-    device_create_info.ppEnabledExtensionNames = &extension_names;
-#endif
+    device_create_info.enabledExtensionCount = extension_count;
+    device_create_info.ppEnabledExtensionNames = extension_names;
 
     // Deprecated and ignored, so pass nothing.
     device_create_info.enabledLayerCount = 0;
@@ -293,7 +305,11 @@ b8 select_physical_device(vulkan_context* context) {
         // NOTE: Enable this if compute will be required.
         // requirements.compute = TRUE;
         requirements.sampler_anisotropy = TRUE;
+    #ifdef SLN_PLATFORM_MACOS
+        requirements.discrete_gpu = FALSE;
+    #else
         requirements.discrete_gpu = TRUE;
+    #endif
         requirements.device_extension_names = darray_create(const char*);
         darray_push(requirements.device_extension_names, &VK_KHR_SWAPCHAIN_EXTENSION_NAME);
 
